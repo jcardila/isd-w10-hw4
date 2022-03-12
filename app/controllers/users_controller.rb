f77c20280b192783447ffdb7238c3d5727b8dac0
jcardila <andrea.rueda@kellogg.northwestern.edu>
2022-03-12 13:00:41 +0000

    user auth - encrypt password, unique email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,15 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params["user"])
-    @user.save
-    redirect_to "/"
+    @user.password = BCrypt::Password.create(params["user"]["password"])
+    if User.where({ email: params["user"]["email"] }).count > 0
+      flash[:notice] = "Email already registered, use a different email address."
+      redirect_to "/users/new"
+    else
+      @user.save
+      redirect_to "/"
+    end
+
+    
   end
 end
