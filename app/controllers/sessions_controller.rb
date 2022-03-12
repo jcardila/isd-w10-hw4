7c88c4f70ae0b19dbeea7dcf40461d303e405824
jcardila <andrea.rueda@kellogg.northwestern.edu>
2022-03-12 13:06:42 +0000

    user auth - login form, auth and create session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.where({ email: params["email"] })[0]
+    if @user
+      if BCrypt::Password.new(@user.password) == params["password"]
+        session["user_id"] = @user.id
+        flash[:notice] = "You logged in!"
+        redirect_to "/"
+      else
+          flash[:notice] = "Error logging in!"
+          redirect_to "/sessions/new"
+      end
+    else
+      flash[:notice] = "Error logging in!"
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
